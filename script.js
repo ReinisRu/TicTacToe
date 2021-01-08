9ae8e65030ca7ffac63af5a1d2f56d30f10bc658
ReinisRu <rr@itfly.lv>
2021-01-09 00:11:49 +0200

    JS "restart" fixed, "stopgame" added

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-// let cells = document.querySelectorAll(".cell");
-
-// for (let cell of cells) {
-//   cell.addEventListener("click", function () {
-//     cell.querySelector("span").textContent = "+";
-//   });
-// }
-/// ----------------
 const playerOne = `X`;
 const playerTwo = `O`;
 const btnRestart = document.getElementById(`btnRestart`);
 let currentPlayer = playerOne;
 let message = document.getElementById(`message`);
+let gameOver = false;
 
 let board = document.querySelector(`#board`);
 for (let i = 0; i <= 8; i++) {
@@ -25,24 +18,29 @@ const cells = Array.from(document.querySelectorAll(`.cell`));
 console.log(cells);
 
 const cellClicked = function (e) {
-  const id = e.target.id;
-  if (!cells[id]) {
-    cells[id] = currentPlayer;
+  let cellText = e.target.value;
+  if (!cellText) {
+    cellText = currentPlayer;
     e.target.innerText = currentPlayer;
-    console.log(cells[id]);
+    e.target.removeEventListener("click", cellClicked);
     if (playerWon()) {
-      message.innerText = `${currentPlayer} won the game!`;
-      return;
+      message.innerText = `🏆 ${currentPlayer} won the game!`;
+      gameOver = true;
+      stopGame();
     }
     currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
   }
 };
 
-const restartGame = function () {
+function restartGame() {
   cells.forEach((cell) => cell.addEventListener(`click`, cellClicked));
-};
+}
 
 restartGame();
+
+function stopGame() {
+  cells.forEach((cell) => cell.removeEventListener(`click`, cellClicked));
+}
 
 const playerWon = function () {
   if (
@@ -50,48 +48,72 @@ const playerWon = function () {
     cells[1].innerText === currentPlayer &&
     cells[2].innerText === currentPlayer
   ) {
+    cells[0].style.color = "#82b74b";
+    cells[1].style.color = "#82b74b";
+    cells[2].style.color = "#82b74b";
     return true;
   } else if (
     cells[3].innerText === currentPlayer &&
     cells[4].innerText === currentPlayer &&
     cells[5].innerText === currentPlayer
   ) {
+    cells[3].style.color = "#82b74b";
+    cells[4].style.color = "#82b74b";
+    cells[5].style.color = "#82b74b";
     return true;
   } else if (
     cells[6].innerText === currentPlayer &&
     cells[7].innerText === currentPlayer &&
     cells[8].innerText === currentPlayer
   ) {
+    cells[6].style.color = "#82b74b";
+    cells[7].style.color = "#82b74b";
+    cells[8].style.color = "#82b74b";
     return true;
   } else if (
     cells[0].innerText === currentPlayer &&
     cells[3].innerText === currentPlayer &&
     cells[6].innerText === currentPlayer
   ) {
+    cells[0].style.color = "#82b74b";
+    cells[3].style.color = "#82b74b";
+    cells[6].style.color = "#82b74b";
     return true;
   } else if (
     cells[1].innerText === currentPlayer &&
     cells[4].innerText === currentPlayer &&
     cells[7].innerText === currentPlayer
   ) {
+    cells[1].style.color = "#82b74b";
+    cells[4].style.color = "#82b74b";
+    cells[7].style.color = "#82b74b";
     return true;
   } else if (
     cells[2].innerText === currentPlayer &&
     cells[5].innerText === currentPlayer &&
     cells[8].innerText === currentPlayer
   ) {
+    cells[2].style.color = "#82b74b";
+    cells[5].style.color = "#82b74b";
+    cells[8].style.color = "#82b74b";
     return true;
   } else if (
     cells[0].innerText === currentPlayer &&
     cells[4].innerText === currentPlayer &&
     cells[8].innerText === currentPlayer
   ) {
+    cells[0].style.color = "#82b74b";
+    cells[4].style.color = "#82b74b";
+    cells[8].style.color = "#82b74b";
     return true;
   } else if (
     cells[2].innerText === currentPlayer &&
     cells[4].innerText === currentPlayer &&
     cells[6].innerText === currentPlayer
   ) {
+    cells[2].style.color = "#82b74b";
+    cells[4].style.color = "#82b74b";
+    cells[6].style.color = "#82b74b";
     return true;
   }
 };
@@ -99,8 +121,10 @@ const playerWon = function () {
 btnRestart.addEventListener(`click`, function () {
   cells.forEach((cell) => {
     cell.innerText = ``;
+    cell.style.color = "#fff";
   });
   message.innerText = `Play a game`;
   currentPlayer = playerOne;
+  gameOver = false;
   restartGame();
 });
